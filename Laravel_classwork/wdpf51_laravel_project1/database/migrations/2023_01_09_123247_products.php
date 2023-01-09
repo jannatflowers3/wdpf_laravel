fdeb665e6e0af765e8f63735207032f2f65366ea
jannatflowers3 <jannatflowerweb@gmail.com>
2023-01-09 19:05:13 +0600

    first commit

---
+++ Laravel_classwork/wdpf51_laravel_project1/database/migrations/2023_01_09_123247_products.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Products extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id',5);
+            $table->string('product_name',150);
+            $table->string('product_descriptions',300);
+            $table->double('product_price',6,2);
+            $table->tinyInteger('product_category');
+            $table->string('product_img');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+       
+    }
+}
